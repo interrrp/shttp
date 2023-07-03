fc3dc133b289c338ac857e5710ad6d5954fad6c7
watchYourCache <watchYourCache@gmail.com>
2023-07-03 20:15:46 +0800

    Add query parameters option

--- shttp/__main__.py
+++ shttp/__main__.py
@@ -9,12 +9,14 @@ def main(
     url: str,
     method: HTTPMethod = Option(HTTPMethod.GET, "--method", "-m"),
     headers: list[str] = Option([], "--header", "-h"),
+    params: list[str] = Option([], "--param", "-p"),
     body: str = Option(None, "--body", "-b"),
 ) -> None:
     response = requests.request(
         url=url,
         method=method,
         headers=dict(header.split(":") for header in headers),
+        params=dict(param.split("=") for param in params),
         data=body,
     )
 

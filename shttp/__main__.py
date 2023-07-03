4b28863ba8c9e1672de4cb85f4d0baccd4f567af
watchYourCache <watchYourCache@gmail.com>
2023-07-03 20:05:17 +0800

    Setup the CLI

--- shttp/__main__.py
+++ shttp/__main__.py
@@ -1,2 +1,20 @@
+import typer
+from typer import Option
+
+from shttp.types import HTTPMethod
+
+
+def main(
+    url: str,
+    method: HTTPMethod = Option(HTTPMethod.GET, "--method", "-m"),
+    headers: list[str] = Option([], "--header", "-h"),
+    data: str = Option(None, "--data", "-d"),
+) -> None:
+    print(f"URL: {url}")
+    print(f"Method: {method}")
+    print(f"Headers: {headers}")
+    print(f"Data: {data}")
+
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    typer.run(main)
